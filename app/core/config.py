443197bb39cd77046d684a855301112110098792
aavzpy2023 <131402389+aavzpy2023@users.noreply.github.com>
2025-06-19 20:29:49 +0000

    update config

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "DocuHub Interno"
+    PROJECT_VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
 
     # Lee desde las variables de entorno inyectadas por Docker Compose
